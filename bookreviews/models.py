066415575b68bc942b94049a2a31e6dc75ce093f
vicgoodall <vic.goodall@icloud.com>
2023-03-11 19:14:07 +0000

    added a role directly into the table for user (this will not be accessible by users on GUI ever) for type teacher; then was able to default set-up any teacher to the role 1. successfully tested registering a new teacher, and alert occurred when pre-existing email tried to register again

--- bookreviews/models.py
+++ bookreviews/models.py
@@ -1,47 +1,43 @@
 from bookreviews import db
 
 
-class Book(db.Model):
+class Books(db.Model):
     # schema for the Book model
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     author_first = db.Column(db.String(30), nullable=False)
     author_surname = db.Column(db.String(50), nullable=False)
     reviews = db.relationship(
-        "Review", backref="book_reviewed", cascade="all, delete", lazy=True)
+        "Reviews", backref="book_reviewed", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.title
 
 
-class Role(db.Model):
+class Roles(db.Model):
     # schema for the user role model
     id = db.Column(db.Integer, primary_key=True)
     role_name = db.Column(db.String(25), unique=True)
-    teachers = db.relationship(
-        "Teacher", backref="role_teacher", cascade="all, delete", lazy=True)
-    students = db.relationship(
-        "Student", backref="role_student", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.role_name
 
 
-class Teacher(db.Model):
+class Teachers(db.Model):
     # schema for the teacher model
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(40), unique=True, nullable=False)
     prefix = db.Column(db.String(8), nullable=False)
     surname = db.Column(db.String(30), nullable=False)
     school = db.Column(db.String(50), nullable=False)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     role = db.Column(
         db.Integer, db.ForeignKey(
-            "role.id", ondelete="CASCADE"), nullable=False)
+            "roles.id", ondelete="CASCADE"), nullable=False)
     students = db.relationship(
-        "Student", backref="their_teacher", cascade="all, delete", lazy=True)
+        "Students", backref="their_teacher", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
@@ -50,7 +46,7 @@ class Teacher(db.Model):
         )
 
 
-class Student(db.Model):
+class Students(db.Model):
     # schema for the student model
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -58,33 +54,33 @@ class Student(db.Model):
     surname_initial = db.Column(db.String(2), nullable=False)
     school = db.Column(db.String(50), nullable=False)
     books_read = db.Column(db.Integer)
-    password = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     role = db.Column(
         db.Integer, db.ForeignKey(
-            "role.id", ondelete="CASCADE"), nullable=False)
+            "roles.id", ondelete="CASCADE"), nullable=False)
     teacher = db.Column(
         db.Integer, db.ForeignKey(
-            "teacher.id", ondelete="CASCADE"), nullable=False)
+            "teachers.id", ondelete="CASCADE"), nullable=False)
     reviews = db.relationship(
-        "Review", backref="review_author", cascade="all, delete", lazy=True)
+        "Reviews", backref="review_author", cascade="all, delete", lazy=True)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
         return self.first_name + " " + self.surname_initial
 
 
-class Review(db.Model):
+class Reviews(db.Model):
     # schema for the review model
     id = db.Column(db.Integer, primary_key=True)
     reviewer = db.Column(
         db.Integer, db.ForeignKey(
-            "student.id", ondelete="CASCADE"), nullable=False)
+            "students.id", ondelete="CASCADE"), nullable=False)
     title = db.Column(db.String(100), nullable=False)
     review = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
     book = db.Column(
         db.Integer, db.ForeignKey(
-            "book.id", ondelete="CASCADE"), nullable=False)
+            "books.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
         # __repr__ to represent itself in the form of a string
--- bookreviews/routes.py
+++ bookreviews/routes.py
@@ -1,7 +1,7 @@
 from flask import (Flask, render_template, session, url_for, redirect,
                    request, flash)
 from bookreviews import app, db
-from bookreviews.models import Book, Role, Teacher, Student, Review
+from bookreviews.models import Books, Roles, Teachers, Students, Reviews
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -9,33 +9,33 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def home():
     return render_template("base.html")
 
-
+# register new user
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # check if username already exists in db
-        # name_attempt = request.form.get("username").lower()
-        # if (Teacher.query().filter_by(
-        # username == name_attempt)) is not None:
-
         user = request.form["email"]
-        found_user = Teacher.query.filter_by(email=user).first()
+        # first query to check if user already exists
+        found_user = Teachers.query.filter_by(email=user).first()
         if found_user:
             flash("User already exists with this email address.")
             return redirect(url_for("register"))
+        # if no email matches, register new user by adding to db
         else:
-            new_user = Teacher(
+            new_user = Teachers(
                 email=request.form.get("email").lower(),
                 prefix=request.form.get("prefix").lower(),
                 surname=request.form.get("surname").lower(),
                 school=request.form.get("school").lower(),
+                role=1,
                 password=generate_password_hash(request.form.get("password")))
 
             db.session.add(new_user)
             db.session.commit()
 
         # put the new user into 'session' cookie
-            session["user"] = request.form.get("username").lower()
+            session["new_user"] = request.form.get("email").lower()
             flash("Registration Successful")
-            return redirect(url_for("account", email=session["email"]))
+            return redirect(url_for("home"))
+    # NEED TO RETURN TO THIS SECTION TO ROUTE BACK TO ACCOUNT PAGE WHEN THAT IS SET UP!
+
     return render_template("register.html")
